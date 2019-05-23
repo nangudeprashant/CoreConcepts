b51b3a8f52fee8e838c1cbc13a99bfdf496a8f73
Administrator <Administrator@DELL>
2019-05-23 21:12:25 +0530

    Initial commit.

---
+++ CoreConcepts/src/main/java/com/javalive/CoreConcepts/App.java
@@ -0,0 +1,13 @@
+package com.javalive.CoreConcepts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ CoreConcepts/src/main/java/com/javalive/CoreConcepts/LoggingDemo.java
@@ -0,0 +1,16 @@
+package com.javalive.CoreConcepts;
+import org.apache.log4j.Logger;
+
+public class LoggingDemo {
+    static Logger logger = Logger.getLogger(LoggingDemo.class);
+    public static void main(String[] args) {        
+        logger.debug("This is debug message");
+        logger.info("This is info message");
+        logger.warn("This is warn message");
+        //logger.fatal("This is fatal message");
+        //logger.error("This is error message");
+        System.out.println("Logic executed successfully....");
+    }
+}
+
+
