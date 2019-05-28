b791523f0f5dc6db4e2d5ef49e8086600f12e636
Administrator <Administrator@DELL>
2019-05-28 14:22:53 +0530

    Logging related very important changes done.

--- CoreConcepts/src/main/java/com/javalive/CoreConcepts/LoggingDemo.java
+++ CoreConcepts/src/main/java/com/javalive/CoreConcepts/LoggingDemo.java
@@ -1,15 +1,22 @@
 package com.javalive.CoreConcepts;
-import org.apache.log4j.Logger;
+//import org.apache.log4j.Logger;   // Log4J
+import org.slf4j.Logger;			// SLF4J
+import org.slf4j.LoggerFactory; 	// SLF4J
+
 
 public class LoggingDemo {
-    static Logger logger = Logger.getLogger(LoggingDemo.class);
+   // static Logger logger = Logger.getLogger(LoggingDemo.class);// Log4J
+    
+    private static final Logger logger = LoggerFactory.getLogger(LoggingDemo.class);  // SLF4J
+    
     public static void main(String[] args) {        
         logger.debug("This is debug message");
         logger.info("This is info message");
         logger.warn("This is warn message");
+        logger.info("Testing 1234");
         //logger.fatal("This is fatal message");
         //logger.error("This is error message");
-        System.out.println("Logic executed successfully....");
+        System.out.println("\nLogic executed successfully....");
     }
 }
 
